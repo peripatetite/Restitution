b12890ce2ec2366419ff12a4e0255014b7eb4008
peripatetite <irl3w1s@gmail.com>
2022-11-30 12:44:30 -0500

    Player camera and rotating

--- Restitution/Assets/Scripts/David.cs
+++ Restitution/Assets/Scripts/David.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class David : MonoBehaviour
 {
     private Animator davidAnimator;
-    private CharacterController character_controller;
-    public float walking_velocity;
+    private CharacterController characterController;
+    public float walkingVelocity;
     public float velocity;
-    public Vector3 movement_direction;
+    public Vector3 movementDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         davidAnimator = GetComponent<Animator>();
-        character_controller = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
         velocity = 0.0f;
-        walking_velocity = 1.5f;
-        movement_direction = new Vector3(0.0f, 0.0f, 0.0f);
+        walkingVelocity = 1.5f;
+        movementDirection = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -25,48 +25,58 @@ public class David : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) 
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
             {
+                //Crouch walk forwards
+                Debug.Log(characterController.bounds.center);
                 davidAnimator.SetInteger("movement", 3);
-            } else if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                velocity = Mathf.Min(velocity + 0.25f, walkingVelocity / 2);
+            }
+            else if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
+                //Run
                 davidAnimator.SetInteger("movement", 2);
+                velocity = Mathf.Min(velocity + 1, walkingVelocity * 2);
             }
-            else 
+            else
             {
+
+                Debug.Log(characterController.bounds.center);
+                //Walk
                 davidAnimator.SetInteger("movement", 1);
+                velocity = Mathf.Min(velocity + 0.5f, walkingVelocity);
             }
-        } else if (Input.GetKey(KeyCode.C)) 
-        {
-            davidAnimator.SetInteger("movement", 4);    
         }
         else
         {
-            davidAnimator.SetInteger("movement", 0);
-        } 
+            velocity = 0;
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            {
+                //Crouch Idle
+                davidAnimator.SetInteger("movement", 4);
+            }
+            else
+            {
+                //Idle
+                davidAnimator.SetInteger("movement", 0);
+            }
+        }
 
-        if (davidAnimator.GetCurrentAnimatorStateInfo(0).IsName("CrouchingForward")) 
+        //Rotate David right
+        if (Input.GetKey(KeyCode.D))
         {
-            velocity += 0.1f;
-            velocity = (velocity > 0.5f * walking_velocity) ? 0.75f : velocity;
+            transform.Rotate(new Vector3(0, 5f, 0), Space.Self);
         }
-        else if (davidAnimator.GetCurrentAnimatorStateInfo(0).IsName("Running"))
+        //Rotate David left
+        if (Input.GetKey(KeyCode.A))
         {
-            velocity += 0.3f;
-            velocity = (velocity > 2.0f * walking_velocity) ? 3.0f : velocity;
-        }
-        else if (davidAnimator.GetCurrentAnimatorStateInfo(0).IsName("Walking")) 
-        {
-            velocity += 0.1f;
-            velocity = (velocity > walking_velocity) ? 1.5f : velocity;
-        } else {
-            velocity = 0.0f;
+            transform.Rotate(new Vector3(0, -5f, 0), Space.Self);
         }
 
         float xdirection = Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
         float zdirection = Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
-        movement_direction = new Vector3(xdirection, 0.0f, zdirection);
+        movementDirection = new Vector3(xdirection, 0.0f, zdirection);
 
-        character_controller.Move(movement_direction * velocity * Time.deltaTime);
+        characterController.Move(movementDirection * velocity * Time.deltaTime);
     }
 }
