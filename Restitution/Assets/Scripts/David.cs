6ddc22fcab00255cf7c6f4505f657603f6ac4650
peripatetite <irl3w1s@gmail.com>
2022-12-07 11:31:25 -0500

    Tweaks

--- Restitution/Assets/Scripts/David.cs
+++ Restitution/Assets/Scripts/David.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class David : MonoBehaviour
 {
-    private Animator davidAnimator;
-    private CharacterController davidController;
     public float walkingVelocity;
     public float velocity;
     public Vector3 movementDirection;
 
     public bool caught;
+
+    private Animator davidAnimator;
+    private CharacterController davidController;
+
+    private bool triggered;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +30,11 @@ public class David : MonoBehaviour
     {
         if (caught)
         {
-            davidAnimator.SetTrigger("hasLost");
+            if (!triggered)
+            {
+                davidAnimator.SetTrigger("hasLost");
+                triggered = true;
+            }
             return;
         }
         if (Input.GetKey(KeyCode.Space))
