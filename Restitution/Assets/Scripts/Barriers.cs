cceead6797131397824637ca1934c23a9aa233c7
Aditya Roy Chowdhury <adityaroychowdhury@vl965-172-31-7-18.wireless.umass.edu>
2022-12-19 18:37:37 -0500

    Laser Barrier

---
+++ Restitution/Assets/Scripts/Barriers.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barriers : MonoBehaviour
+{
+    private GameObject Barrier1;
+    public bool disabled1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Barrier1 = GameObject.Find("Barrier1");
+        disabled1 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (disabled1)
+        {
+            Transform[] children = Barrier1.GetComponentsInChildren<Transform>();
+            foreach (Transform child in children)
+            {
+                if (child.name.Contains("Beam"))
+                {
+                    Renderer beamRenderer = child.GetComponent<Renderer>();
+                    beamRenderer.enabled = false;
+                }
+            }
+        }
+
+        Barrier1.GetComponent<Collider>().enabled = false;;
+    }
+}
