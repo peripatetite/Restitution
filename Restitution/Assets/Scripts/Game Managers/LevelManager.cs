ecd85f9e5bdba709bfd6e8083dff841102442664
GuangchenW <guangchenwa@gmail.com>
2022-12-20 12:00:52 -0500

    Added pause functionality

--- Restitution/Assets/Scripts/Game Managers/LevelManager.cs
+++ Restitution/Assets/Scripts/Game Managers/LevelManager.cs
@@ -10,13 +10,29 @@ public class LevelManager : MonoBehaviour {
     public GameObject replayButton;
     public GameObject player;
     public Image fade;
+    public GameObject pauseText;
     public string nextLevel;
+    public bool paused;
+
+    private David david;
 
 	private void Awake() {
         instance = this;
 	}
 
-    public void AdvanceLevel() {
+	private void Start() {
+        david = player == null ? null : player.GetComponent<David>();
+	}
+
+	private void Update() {
+		if (david != null && !david.caught && Input.GetKeyDown(KeyCode.P)) {
+            paused = !paused;
+            Time.timeScale = paused ? 0 : 1;
+            pauseText.SetActive(paused);
+		}
+	}
+
+	public void AdvanceLevel() {
         if (player != null)
             player.GetComponent<David>().enabled = false;
         fade.color = new Color(0, 0, 0, 0);
